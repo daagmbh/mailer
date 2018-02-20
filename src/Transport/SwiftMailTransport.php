561bb68550197c621dfc5b872cf722fc7c72e0fc
Markus Fasselt <markus.fasselt@gmail.com>
2018-02-20 15:12:06 +0100

    Make SwiftMailTransport compatible to swiftmailer 6.0

--- src/Transport/SwiftMailTransport.php
+++ src/Transport/SwiftMailTransport.php
@@ -36,7 +36,7 @@ class SwiftMailTransport implements TransportInterface
      */
     public function convertMail(MailInterface $mail)
     {
-        $swiftMessage = Swift_Message::newInstance($mail->getSubject());
+        $swiftMessage = new Swift_Message($mail->getSubject());
         $swiftMessage->setFrom($mail->getSender()->getEmail(), $mail->getSender()->getName());
 
         $recipients = $mail->getRecipients();
@@ -52,7 +52,7 @@ class SwiftMailTransport implements TransportInterface
         }
 
         foreach ($mail->getAttachments() as $attachment) {
-            $swiftAttachment = Swift_Attachment::newInstance(
+            $swiftAttachment = new Swift_Attachment(
                 $attachment->getContent(),
                 $attachment->getFilename(),
                 $attachment->getContentType()
@@ -74,12 +74,12 @@ class SwiftMailTransport implements TransportInterface
         /** @var SmtpSender $sender */
         $sender = $mail->getSender();
 
-        $transport = Swift_SmtpTransport::newInstance($sender->getHost(), $sender->getPort())
+        $transport = (new Swift_SmtpTransport($sender->getHost(), $sender->getPort()))
             ->setUsername($sender->getUser())
             ->setPassword($sender->getPassword())
             ->setEncryption($sender->getEncryption());
 
-        $mailer = Swift_Mailer::newInstance($transport);
+        $mailer = new Swift_Mailer($transport);
 
         return $mailer;
     }
